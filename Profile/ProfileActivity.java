0b8dc680a761a875e3dc936cdfd1989c64cda1bd
sybertDonald <sy bertnkingwa@gmail.com>
2018-12-13 14:23:22 +0300

    new

---
+++ Profile/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.mbisu.tradetool.Profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mbisu.tradetool.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_layout);
+    }
+}
---
+++ StartDay/RegisterStockActivity.java
@@ -0,0 +1,15 @@
+package com.example.mbisu.tradetool.StartDay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mbisu.tradetool.R;
+
+public class RegisterStockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_stock_layout);
+    }
+}
